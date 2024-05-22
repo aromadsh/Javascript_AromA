375775bd87387c1c27b49e2cf8b1512046b242ab
AromA <AromADSH@gmail.com>
2024-05-22 22:19:16 +0900

    today

--- js/Variable_Type/Empty_Object.js
+++ js/Variable_Type/Empty_Object.js
@@ -1,6 +1,19 @@
+const o1 = new Object();
+const o2 = {};
+
+console.log(o1 == o2);
+console.log(o1 === o2);
+console.log(`ol의 값은 ${o1}이며, 데이터 타입은 ${typeof(o1)}`);
+console.log(`o2의 값은 ${o2}이며, 데이터 타입은 ${typeof(o2)}`);
+/*
+o1은 클래스에서 받아온 인스턴스이기 때문에 서로 다르다.
+ */
+
+
 const user = {
     id : 1,
     name : 'Hong',
     get : function(obj) { return `${obj.id} : ${obj.name}`;},
     getInfo() { return `${this.id} - ${this.name}`;}
 };
+console.log(user);
--- js/연산자/반복문.js
+++ js/연산자/반복문.js
@@ -11,5 +11,36 @@ while (i-- > 0) console.log("🚀 ~ i:", i);
 
 
 i = 5;
-do console.log("🚀 ~ i:", i);
-while (i-- > 1);
+do {
+    console.log("🚀 ~ i:", i);
+} while (i-- > 1);
+
+
+for (let j = 1; j <= 5; j++) {
+    console.log(j);
+}
+
+for (let j = 5; j > 0; j--) {
+    console.log(j);
+}
+
+
+const arr = [2, 3, 5, 7, 11];
+
+for(let i = 0; i < arr?.length; i++) {
+    console.log(`arr[${i}] : `, arr[i]);
+}
+
+for(const t of arr) {
+    console.log(t);
+}
+
+const user = { id : 1, name : 'Hong'};
+for(const p in user) {
+    console.log(user[p]);
+}
+
+const WEEK_NAMES = '월화수목금토일';
+for (const w of WEEK_NAMES) {
+    console.log(w);
+}
